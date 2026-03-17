b100dbba36b1a0c6f44344274ae39ececf443f74
carlbenson <billonesbenson@gmail.com>
2026-03-17 15:41:35 +0000

    Intentionally adding error to demonstrate rollback

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#!/bin/bash
+# Add this at the top of your script to make it exit immediately if any command fails
+set -e
+
+# Your intentional error
+sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
+
+# To be extra sure, we can also add an explicit exit code
+exit 1
+
+
 isExistApp="$(pgrep httpd)"
 if [[ -n $isExistApp ]]; then
 sudo systemctl stop httpd.service
